34e7b8ac2ac7225ffff5319ec3fd3d34a30c133b
AnthonyStembreit <stembreit@gmail.com>
2022-04-09 11:38:22 -0500

    added cors to attempt to resolve cors error

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
+const cors = require("cors")
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors())
+
 app.use(routes);
 
 db.once('open', () => {
